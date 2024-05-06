ad82dd5f8c296d4971d06aab44f3faca0df72c61
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2024-05-07 02:35:26 +0300

    Init

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from "node:fs";
+import { defineConfig } from "vite";
+
+// Vite plugins
+import vue from "@vitejs/plugin-vue";
+import webExtension from "vite-plugin-web-extension";
+
+// Utilities
+import { readJsonFile as json } from "vite-plugin-web-extension";
+import { parse as parseYaml } from "yaml";
+
+const yaml = (path: string) => parseYaml(readFileSync(path, "utf-8"));
+
+const generateManifest = () => {
+  const manifest = yaml("src/manifest.yml");
+  const pkg = json("package.json");
+
+  return {
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    webExtension({
+      manifest: generateManifest,
+      watchFilePaths: ["package.json", "src/manifest.yml"],
+      additionalInputs: ["src/content.ts"],
+      browser: "firefox"
+    }),
+  ],
+});
